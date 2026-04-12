786fbc7d61bc42830b2c56d322c3c4970860801e
Bhaumik <bhaumik182001@gmail.com>
2026-04-12 19:40:58 +0530

    updated security configuration to allow hosted vercel frontend

--- src/main/java/com/bhaumik18/medisync_core/config/SecurityConfig.java
+++ src/main/java/com/bhaumik18/medisync_core/config/SecurityConfig.java
@@ -48,7 +48,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); 
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", 
+                "https://medisync-frontend-vert.vercel.app")); 
         // Explicitly allowing OPTIONS and DELETE
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
